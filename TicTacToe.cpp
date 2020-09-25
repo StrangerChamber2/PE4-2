0629a355ca42cdf5730ccfbbdc00e5760258a6d1
Jack Blackburn <jackblackburn124@gmail.com>
2020-09-25 15:08:22 -0600

    displayboard done

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 
-void createBoard(){
-    std::string board[3][3];
+void createBoard(std::string board[3][3]){
+    for(int o =0;o<3;o++){
+        for(int i = 0;i<3;i++){
+            board[o][i] = "  ";
+        }
+    }    
+}
 
+void displayBoard(std::string board[3][3]){
+    for(int o =0;o<3;o++){
+        for(int i = 0;i<3;i++){
+            std::cout << board[o][i];
+        }
+        std::cout << std::endl;
+    }
 }
 
 int main(){
-    createBoard();
-    //hello
+    std::string board[3][3];
+    createBoard(board);
+    displayBoard(board);
 }
